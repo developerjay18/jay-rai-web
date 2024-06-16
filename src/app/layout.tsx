c9e225927493ae67bfaf416b9117200f247d90f3
jay rai <jayrai3003@gmail.com>
2024-06-16 20:13:16 +0530

    setting up my base set up

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jay Rai | full stack web developer",
+  description:
+    "Best web developer in ahmedabad | MERN stack developer | freelancer developer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-5xl text-center">
+        Website is Under maintainance <br /> will be published soon...
+      </h1>
+      <h3>- updated on 16/06/2024</h3>
+    </main>
+  );
+}
